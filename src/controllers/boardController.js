fff07de84c28251f031c79e2f95862b944bcf21d
dathuynh1710 <huynhthanhdat2506@gmail.com>
2025-07-07 19:51:17 +0700

    Add service layer

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,12 +1,10 @@
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
   try {
-    // console.log(req.body)
-
-    res.status(StatusCodes.CREATED).json({
-      message: 'POST from boardController'
-    })
+    const createdBoard = await boardService.createNew(req.body)
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) {
     next(error)
   }
