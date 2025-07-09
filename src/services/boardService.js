c3e929cde4906ada7d5efeef23fa65215c47a51e
dathuynh1710 <huynhthanhdat2506@gmail.com>
2025-07-09 13:45:13 +0700

    API create board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,18 +1,38 @@
 /* eslint-disable no-useless-catch */
-import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 const createNew = async (reqBody) => {
   try {
     const newBoard = {
       ...reqBody,
       slug: slugify(reqBody.title)
     }
-    return newBoard
+    // Goi toi tang model de xu ly luu ban ghi newBoard vao trong DB
+    const createdBoard = await boardModel.createNew(newBoard)
+    // console.log(createdBoard)
+
+    // Lấy bản ghi board sau khi gọi
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+
+    return getNewBoard
+  } catch (error) {
+    throw error
+  }
+}
+
+const getDetails = async (boardId) => {
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    return board
   } catch (error) {
     throw error
   }
 }
 
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
