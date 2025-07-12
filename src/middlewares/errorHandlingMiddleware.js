b9208c883b68c094b59110778c057344b844dfd7
dathuynh1710 <huynhthanhdat2506@gmail.com>
2025-07-12 21:14:30 +0700

    Add config CORS

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -15,6 +15,7 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
   }
   // console.error(responseError)
 
+  // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn, còn không thì xóa đi
   if (env.BUILD_MODE !== 'dev') delete responseError.stack
 
   // Đoạn này có thể mở rộng nhiều về sau như ghi Error Log vào file, bắn thông báo lỗi vào group Slack, Telegram, Email...
--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 /* eslint-disable no-console */
 import express from 'express'
+import cors from 'cors'
+import { corsOptions } from '~/config/cors'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
 import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
+  // Xử lý CORS
+  app.use(cors(corsOptions))
+  // Enable req.body json data
   app.use(express.json())
+  // Use APIs V1
   app.use('/v1', APIs_V1)
   // Middleware xử lý lỗi tập trung
   app.use(errorHandlingMiddleware)
