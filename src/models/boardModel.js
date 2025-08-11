0c4d15556d40e0699de872844e83f518d3574c80
dathuynh1710 <huynhthanhdat2506@gmail.com>
2025-08-11 20:00:11 +0700

    Add aggregate

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,17 +1,20 @@
-import Joi from 'joi'
-import { ObjectId } from 'mongodb'
-import { GET_DB } from '~/config/mongodb'
-import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
-const BOARD_COLLECTION_NAME = 'boards'
+import Joi from "joi"
+import { ObjectId } from "mongodb"
+import { GET_DB } from "~/config/mongodb"
+import { BOARD_TYPES } from "~/utils/constants"
+import { columnModel } from "~/models/columnModel"
+import { cardModel } from "~/models/cardModel"
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators"
+const BOARD_COLLECTION_NAME = "boards"
 const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
-
+  type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required(),
   columnOrderIds: Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)).default([]),
 
-  createdAt: Joi.date().timestamp('javascript').default(Date.now()),
-  updatedAt: Joi.date().timestamp('javascript').default(null),
+  createdAt: Joi.date().timestamp("javascript").default(Date.now()),
+  updatedAt: Joi.date().timestamp("javascript").default(null),
   _destroy: Joi.boolean().default(false)
 })
 
@@ -22,7 +25,7 @@ const validateBeforeCreate = async (data) => {
 const createNew = async (data) => {
   try {
     const validData = await validateBeforeCreate(data)
-    console.log('validData', validData)
+    console.log("validData", validData)
     const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validData)
     return createdBoard
   } catch (error) {
@@ -39,13 +42,40 @@ const findOneById = async (id) => {
     throw new Error(error)
   }
 }
-
+// Query tổng hợp (aggregate) để lấy toàn bộ Columns và Cards thuộc về Board
 const getDetails = async (id) => {
   try {
+    // const result = await GET_DB()
+    //   .collection(BOARD_COLLECTION_NAME)
+    //   .findOne({ _id: new ObjectId(id) })
     const result = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
-      .findOne({ _id: new ObjectId(id) })
-    return result
+      .aggregate([
+        {
+          $match: {
+            _id: new ObjectId(id),
+            _destroy: false
+          }
+        },
+        {
+          $lookup: {
+            from: columnModel.COLUMN_COLLECTION_NAME,
+            localField: "_id",
+            foreignField: "boardId",
+            as: "columns"
+          }
+        },
+        {
+          $lookup: {
+            from: cardModel.CARD_COLLECTION_NAME,
+            localField: "_id",
+            foreignField: "boardId",
+            as: "cards"
+          }
+        }
+      ])
+      .toArray()
+    return result[0] || null
   } catch (error) {
     throw new Error(error)
   }
