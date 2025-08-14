54fbf37d21484cf616b3e21fe56e300954939e13
dathuynh1710 <huynhthanhdat2506@gmail.com>
2025-08-14 10:59:57 +0700

    Add drag-and-drop cards integration with API #1

--- src/routes/v1/columnRoute.js
+++ src/routes/v1/columnRoute.js
@@ -3,5 +3,6 @@ import { columnValidation } from '~/validations/columnValidation'
 import { columnController } from '~/controllers/columnController'
 const Router = express.Router()
 Router.route('/').post(columnValidation.createNew, columnController.createNew)
+Router.route('/:id').put(columnValidation.update, columnController.update)
 
 export const columnRoute = Router
