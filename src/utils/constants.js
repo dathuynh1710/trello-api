bff0fdbdea83ec609bd056ea727fe77da72740a7
dathuynh1710 <huynhthanhdat2506@gmail.com>
2025-08-18 10:39:42 +0700

    Add WHITELIST DOMAINS again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // Những domain được phép truy cập tới tài nguyên server
-export const WHITELIST_DOMAINS = ['https://trello-web-nine-rho.vercel.app/']
+export const WHITELIST_DOMAINS = ['https://trello-web-nine-rho.vercel.app']
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
